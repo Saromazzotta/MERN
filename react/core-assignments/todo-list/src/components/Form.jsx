b336f09271827df597c7e43fec2ac574eee3356b
Saro Mazzotta <Saromazzotta@gmail.com>
2023-03-02 22:01:14 -0500

    started todo list core assignment

---
+++ react/core-assignments/todo-list/src/components/Form.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Form = () => {
+    return (
+        <div>
+            <div className="form col-mod-4 mx-auto mt-3">
+                <div className="form-group">
+                    <input type="text" className="form-control-sm" />
+                </div>
+                <button className="btn-primary mt-3">Add</button>
+            </div>
+        </div>
+    )
+}
+
+export default Form
