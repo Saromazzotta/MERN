e705f7146e7ffbc454bc9fdf39af2ec58bd78c14
Saro Mazzotta <Saromazzotta@gmail.com>
2023-03-11 10:05:28 -0500

    completed pokemon api practice assignment

--- react/practice-assignments/pokemon-api/src/App.js
+++ react/practice-assignments/pokemon-api/src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const base_url = "https://pokeapi.co/api/v2/";
+  const [pokemons, setPokemons] = useState([]);
+
+  useEffect(() => {
+    fetch(base_url + "pokemon?limit=807")
+      .then(response => response.json())
+      .then(json => setPokemons(json.results))
+      .catch(error => console.log(error));
+  }, []);
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {
+        pokemons.map(pokemon => (
+          <div key={pokemon.id}>
+            <ul style={{display: 'flex', justifyContent: 'center'}}>
+              <li>{pokemon.name}</li>
+            </ul>
+          </div>
+        ))
+      }
     </div>
   );
 }
