df98f0f78bb711ea0eb7201b268d5c2b59d59f26
Saromazzotta <Saromazzotta@gmail.com>
2023-03-29 12:55:57 -0400

    product manager assignment

---
+++ fullStackMERN/product-manager/client/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import { Routes, Route } from 'react-router-dom';
+
+function App() {
+  return (
+    <div className="App">
+      <Routes>
+        <Route element={} path="api/players/create" />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
---
+++ fullStackMERN/product-manager/server/controllers/product.controller.js
@@ -0,0 +1,17 @@
+const Product = require('../models/product.model');
+
+// Find all products
+// module.exports.findAllProducts = (req, res) => {
+//     Product.find()
+//         .then(allProducts => res.json({ products: allProducts }))
+//         .catch(err => res.json({ message: "Something went wrong", error: err }))
+// }
+
+
+// Create product
+module.exports.createProduct = (req, res) => {
+    Product.create(req.body)
+        .then(newProduct => res.json({ product: newProduct }))
+        .catch(err => res.json({ message: "Something went wrong,", error: err }))
+}
+
